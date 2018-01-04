786c89792b4586ddd425804becbee5ca4936b059
arycancer <arycancer@gmail.com>
2018-01-04 09:21:44 +0700

    - add showapiprogressdialog for basefragment

--- coconut-code/src/main/java/app/beelabs/com/codebase/base/response/BaseResponse.java
+++ coconut-code/src/main/java/app/beelabs/com/codebase/base/response/BaseResponse.java
@@ -1,9 +1,12 @@
 package app.beelabs.com.codebase.base.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 /**
  * Created by arysuryawan on 8/19/17.
  */
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class BaseResponse {
 
     private String status;
