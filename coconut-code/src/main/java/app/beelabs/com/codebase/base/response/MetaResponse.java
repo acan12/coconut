bb3f4fc73bd1b6818599f77285a16c4f840255a8
ary <ary@clappingape.com>
2018-08-02 15:25:02 +0700

    add ignoreunknow attribute avoid error

--- coconut-code/src/main/java/app/beelabs/com/codebase/base/response/MetaResponse.java
+++ coconut-code/src/main/java/app/beelabs/com/codebase/base/response/MetaResponse.java
@@ -1,5 +1,8 @@
 package app.beelabs.com.codebase.base.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MetaResponse {
 
     private boolean status;
