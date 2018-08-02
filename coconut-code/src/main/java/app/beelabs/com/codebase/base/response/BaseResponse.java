fde951735568824e0c931726604bc8e190d0e8b3
ary <ary@clappingape.com>
2018-08-02 14:31:33 +0700

    add meta data response model

--- coconut-code/src/main/java/app/beelabs/com/codebase/base/response/BaseResponse.java
+++ coconut-code/src/main/java/app/beelabs/com/codebase/base/response/BaseResponse.java
@@ -31,42 +31,4 @@ public class BaseResponse {
         this.data = data;
     }
 
-    public class MetaResponse {
-
-        private boolean status;
-        private int code;
-        private String message;
-
-        public MetaResponse() {
-        }
-
-        public boolean isStatus() {
-            return status;
-        }
-
-        public void setStatus(boolean status) {
-            this.status = status;
-        }
-
-        public int getCode() {
-            return code;
-        }
-
-        public void setCode(int code) {
-            this.code = code;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-    }
-
-    public class DataResponse {
-        public DataResponse() {
-        }
-    }
 }
---
+++ coconut-code/src/main/java/app/beelabs/com/codebase/base/response/DataResponse.java
@@ -0,0 +1,6 @@
+package app.beelabs.com.codebase.base.response;
+
+public class DataResponse {
+    public DataResponse() {
+    }
+}
