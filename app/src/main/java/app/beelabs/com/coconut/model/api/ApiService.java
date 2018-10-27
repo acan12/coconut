076c547b22cbcf3e838f9f138bdea7c8afc6742d
ary <ary@clappingape.com>
2018-10-27 09:47:48 +0700

    clean code

--- app/src/main/java/app/beelabs/com/coconut/model/api/ApiService.java
+++ app/src/main/java/app/beelabs/com/coconut/model/api/ApiService.java
@@ -3,13 +3,11 @@ package app.beelabs.com.coconut.model.api;
 import app.beelabs.com.coconut.model.api.response.ArticleResponse;
 import app.beelabs.com.coconut.model.api.response.SourceResponse;
 import app.beelabs.com.coconut.model.api.response.SummaryResponse;
-import app.beelabs.com.codebase.base.response.BaseResponse;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
-import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
@@ -26,6 +24,7 @@ public interface ApiService {
             "Content-Type: application/json"})
     @GET("sources")
     Call<SourceResponse> callApiSources(@Query("language") String language);
+
 
     @Headers({
             "Cache-Control: no-cache",
