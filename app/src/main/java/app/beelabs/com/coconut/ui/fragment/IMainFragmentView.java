ee3452d93d80419c63cbd9225c1d4cb8a09def8c
ary <ary@clappingape.com>
2018-11-09 19:39:41 +0700

    clean code

--- app/src/main/java/app/beelabs/com/coconut/ui/fragment/IMainFragmentView.java
+++ app/src/main/java/app/beelabs/com/coconut/ui/fragment/IMainFragmentView.java
@@ -1,7 +1,6 @@
 package app.beelabs.com.coconut.ui.fragment;
 
 import app.beelabs.com.coconut.model.api.response.SummaryResponse;
-import app.beelabs.com.codebase.base.IView;
 
 public interface IMainFragmentView {
 
