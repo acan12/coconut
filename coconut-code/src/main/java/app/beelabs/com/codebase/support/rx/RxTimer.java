33017af1ea25124d1b9091b68bd78326a7606e24
ary <ary@clappingape.com>
2019-03-03 11:59:13 +0700

    update rxtimer

--- coconut-code/src/main/java/app/beelabs/com/codebase/support/rx/RxTimer.java
+++ coconut-code/src/main/java/app/beelabs/com/codebase/support/rx/RxTimer.java
@@ -9,12 +9,12 @@ import io.reactivex.schedulers.Schedulers;
 
 public class RxTimer {
 
-    public static void doTimer(long delay, boolean repeat, final RxTimer callback){
-        Observable<Long> obs = Observable.timer(delay, TimeUnit.MILLISECONDS)
+    public static Observable doTimer(long delay, boolean repeat, final RxTimer callback) {
+        Observable obs = Observable.timer(delay, TimeUnit.MILLISECONDS)
                 .subscribeOn(Schedulers.io());
 
-                if(repeat) obs = obs.repeat();
-                obs.observeOn(AndroidSchedulers.mainThread())
+        if (repeat) obs = obs.repeat();
+        obs.observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Consumer<Long>() {
                     @Override
                     public void accept(Long aLong) throws Exception {
@@ -24,10 +24,12 @@ public class RxTimer {
                     }
 
                 });
+
+        return obs;
     }
 
 
-    public void onCallback(Long along){
+    public void onCallback(Long along) {
 
     }
 }
