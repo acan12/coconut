4a61c4edb2548c05215ad9f48f483fb7848b6723
ary <ary@clappingape.com>
2019-10-11 14:27:25 +0700

    update crypto chiper object for encryption

--- coconut-code/src/main/java/app/beelabs/com/codebase/support/util/CryptoUtil.java
+++ coconut-code/src/main/java/app/beelabs/com/codebase/support/util/CryptoUtil.java
@@ -6,9 +6,12 @@ import android.util.Log;
 import java.security.Key;
 import java.security.KeyFactory;
 import java.security.spec.KeySpec;
+import java.security.spec.MGF1ParameterSpec;
 import java.security.spec.X509EncodedKeySpec;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.OAEPParameterSpec;
+import javax.crypto.spec.PSource;
 
 public class CryptoUtil {
 
@@ -20,11 +23,17 @@ public class CryptoUtil {
             KeySpec keySpec = new X509EncodedKeySpec(Base64.decode(strPublicKey.trim().getBytes(), Base64.DEFAULT));
             Key publicKey = keyFac.generatePublic(keySpec);
 
-            final Cipher cipher = Cipher.getInstance("RSA/NONE/PKCS1Padding");
-            // encrypt the plain text using the public key
-            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            final Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-1AndMGF1Padding");
+            OAEPParameterSpec oaepParams = new OAEPParameterSpec(
+                    "SHA-1", "MGF1",
+                    new MGF1ParameterSpec("SHA-1"),
+                    PSource.PSpecified.DEFAULT
+            );
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey, oaepParams);
             cipherText = cipher.doFinal(text);
             strEncryInfoData = new String(Base64.encode(cipherText, Base64.DEFAULT));
+
+
 
         } catch (Exception e) {
             String m = e.getMessage();
