cbcf1fedcf679d3a10dde671e6d2d727fb95a293
ary <ary@clappingape.com>
2020-06-09 16:04:54 +0700

    fix issue warning code kotlin implementation lib

--- coconut-code/src/main/java/app/beelabs/com/codebase/component/dialog/CoconutAlertInternetNoConnectionDialog.kt
+++ coconut-code/src/main/java/app/beelabs/com/codebase/component/dialog/CoconutAlertInternetNoConnectionDialog.kt
@@ -7,7 +7,7 @@ import app.beelabs.com.codebase.base.BaseDialog
 import app.beelabs.com.codebase.base.contract.IView
 import kotlinx.android.synthetic.main.dialog_coconut_alert_internet_noconnection.*
 
-class CoconutAlertInternetNoConnectionDialog(val message: String, private val iview: IView, style: Int) : BaseDialog(iview as Activity, style) {
+class CoconutAlertInternetNoConnectionDialog(val message: String?, private val iview: IView, style: Int) : BaseDialog(iview as Activity, style) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setWindowContentDialogLayout(R.layout.dialog_coconut_alert_internet_noconnection)
