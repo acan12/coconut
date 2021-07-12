2937caa01233f48b6a983af597adee93339e4d59
ary <ary@clappingape.com>
2021-07-13 06:07:51 +0700

    clean up code + upgrade version 2.3.0-rc.7

--- coconut-code/src/main/java/app/beelabs/com/codebase/base/BaseManager.java
+++ coconut-code/src/main/java/app/beelabs/com/codebase/base/BaseManager.java
@@ -1,14 +1,6 @@
 package app.beelabs.com.codebase.base;
 
-import android.util.Log;
-
-import java.security.KeyManagementException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import javax.net.ssl.HostnameVerifier;
@@ -16,10 +8,8 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
-import app.beelabs.com.codebase.component.interceptor.RSAInterceptor;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -43,50 +33,28 @@ public class BaseManager {
                                          Interceptor[] customNetworkInterceptors) {
         OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
 
-        if (allowUntrustedSSL) {
-            httpClient = allowUntrustedSSL(httpClient);
-        }
-
-
-//        try {
-//            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
-//                    TrustManagerFactory.getDefaultAlgorithm());
-//            trustManagerFactory.init((KeyStore) null);
-//            TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
-//            if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
-//                throw new IllegalStateException("Unexpected default trust managers:"
-//                        + Arrays.toString(trustManagers));
-//            }
-//
-//            X509TrustManager trustManager = (X509TrustManager) trustManagers[0];
-//
-//            SSLContext sc = SSLContext.getInstance("TLSv1.2");
-//            sc.init(null, null, null);
-//            httpClient.sslSocketFactory(new TLS12SocketFactory(sc.getSocketFactory()), trustManager);
-//        } catch (NoSuchAlgorithmException | KeyManagementException e) {
-//            e.printStackTrace();
-//        } catch (KeyStoreException e) {
-//            e.printStackTrace();
-//        }
+        // allowUntrustedSSL: true , if activate Untrusted SSL
+        if (allowUntrustedSSL) httpClient = allowUntrustedSSL(httpClient);
 
         httpClient.connectTimeout(timeout, TimeUnit.SECONDS);
         httpClient.readTimeout(timeout, TimeUnit.SECONDS);
         httpClient.writeTimeout(timeout, TimeUnit.SECONDS);
 
-        // interceptor logging HTTP request
+        // Interceptor logging HTTP request
         if (enableLoggingHttp) {
             HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
             logging.setLevel(HttpLoggingInterceptor.Level.BODY);
             httpClient.addInterceptor(logging);
         }
 
-
+        // Pre-Interceptor
         if (customInterceptors != null) {
             for (Interceptor interceptor : customInterceptors) {
                 httpClient.addInterceptor(interceptor);
             }
         }
 
+        // Post-Network Interceptor
         if (customNetworkInterceptors != null) {
             for (Interceptor interceptor : customNetworkInterceptors) {
                 httpClient.addNetworkInterceptor(interceptor);
@@ -94,45 +62,6 @@ public class BaseManager {
         }
         return httpClient.build();
     }
-
-//    protected OkHttpClient getHttpClient(boolean allowUntrustedSSL,
-//                                         int timeout,
-//                                         boolean enableLoggingHttp,
-//                                         final boolean enableEncryptedRSA) {
-//
-//        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-//
-//        if (allowUntrustedSSL) {
-//            allowUntrustedSSL(httpClient);
-//        }
-//
-//
-//        try {
-//            SSLContext sc = SSLContext.getInstance("TLSv1.2");
-//            sc.init(null, null, null);
-//            httpClient.sslSocketFactory(new TLS12SocketFactory(sc.getSocketFactory()));
-//        } catch (NoSuchAlgorithmException | KeyManagementException e) {
-//            e.printStackTrace();
-//        }
-//
-//        httpClient.connectTimeout(timeout, TimeUnit.SECONDS);
-//        httpClient.readTimeout(timeout, TimeUnit.SECONDS);
-//        httpClient.writeTimeout(timeout, TimeUnit.SECONDS);
-//
-//        // interceptor RSA for body encryption
-//        if (enableEncryptedRSA) {
-//            httpClient.addInterceptor(new RSAInterceptor());
-//        }
-//
-//        // interceptor logging HTTP request
-//        if (enableLoggingHttp) {
-//            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-//            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
-//            httpClient.addInterceptor(logging);
-//        }
-//
-//        return httpClient.build();
-//    }
 
     private OkHttpClient.Builder allowUntrustedSSL(OkHttpClient.Builder httpClient) {
 
