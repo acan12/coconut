d75d28439fe141b5f4208d48156c8b6013797cb1
ary <ary@clappingape.com>
2021-07-13 10:06:32 +0700

    update func for setup font in app

--- coconut-code/src/main/java/app/beelabs/com/codebase/base/BaseApp.java
+++ coconut-code/src/main/java/app/beelabs/com/codebase/base/BaseApp.java
@@ -2,14 +2,10 @@ package app.beelabs.com.codebase.base;
 
 import android.app.Application;
 
-import app.beelabs.com.codebase.R;
 import app.beelabs.com.codebase.di.component.AppComponent;
 import app.beelabs.com.codebase.di.component.DaggerAppComponent;
 import app.beelabs.com.codebase.di.module.ApiServiceModule;
 import app.beelabs.com.codebase.di.module.AppModule;
-import io.github.inflationx.calligraphy3.CalligraphyConfig;
-import io.github.inflationx.calligraphy3.CalligraphyInterceptor;
-import io.github.inflationx.viewpump.ViewPump;
 
 /**
  * Created by arysuryawan on 11/15/17.
@@ -27,16 +23,4 @@ public class BaseApp extends Application {
                 .apiServiceModule(new ApiServiceModule(app))
                 .build();
     }
-
-    protected static void setupDefaultFont(String defaultPath){
-        ViewPump.init(new ViewPump.Builder()
-                .addInterceptor(new CalligraphyInterceptor(
-                        new CalligraphyConfig.Builder()
-                                .setDefaultFontPath(defaultPath)
-                                .setFontAttrId(R.attr.fontPath)
-                        .build()))
-                .build()
-        );
-    }
-
 }
